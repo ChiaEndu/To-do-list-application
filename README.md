6c6cbcda90ee81655b18f8e0f03437935facc1d0
Endurance <endurancechia@gmail.com>
2025-11-24 20:43:27 +0100

    removed .txt files from tracking

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# TO DO LIST
+# TO DO LIST APPLICATION
+
+
+## overview 
+ The TO DO LIST APPLICATION is simple to use app that allows users to track their activities with options to add and delete task
--- todo.sh
+++ todo.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Identify tasks file
+# Identify tasks file 
 TASK_FILE="tasks.txt"
 
 case "$1" in
 add)
-if [ -z "$2" ]; then
+if [ -z "$2" ]; then       # -z means string is empty
 echo "no task added"
 exit 1
 
 fi
 # Add task
-echo "$2" >> "$TASK_FILE"
+
+echo "$2" >> "$TASK_FILE"      # "$2" is the second argument
 echo "task added: $2"
 ;;
 # list task
@@ -21,17 +22,17 @@ echo "no tasks"
 else
 echo "to do list"
 fi
-nl -w1 -s". " "$TASK_FILE"
+nl -w1 -s". " "$TASK_FILE"       # nl is the number line, w1 is the width
 ;;
 # remove tasks
 
 delete)
 if [ -z "$2" ]; then
 echo "task number"
-exit 1
+exit 1                             # exit 1 = if condition is true, the script terminates with status 1
 fi
 sed -i'' "${2}d" "$TASK_FILE"
-echo "deleted task: $2"
+echo "deleted task: $2"               # ${2}d  sed command for delete
 ;;
 *)
 echo "usage: ./todo.sh add \"task description\""
